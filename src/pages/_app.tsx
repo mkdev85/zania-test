6982426fd53d5570b46fa5b65ca64f182c30434f
Mayank <mkdev8585@gmail.com>
2024-07-19 18:39:35 +0530

    Refactored the code and added bootstrap helper method for tracking the data loading.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
+import { useOnMount } from "@cat/hooks/useOnMount";
+import { bootStrap } from "@cat/lib/bootStrap";
+import "@cat/styles/globals.css";
+import { PageLoader } from "@cat/ui-kit/PageLoader/PageLoader";
 import type { AppProps } from "next/app";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isBootstraped, setIsBootstraped] = useState<boolean>(false);
+
+  useOnMount(() => {
+    bootStrap().then(() => {
+      setIsBootstraped(true);
+    });
+  });
+
+  if (!isBootstraped) {
+    return <PageLoader />;
+  }
+
   return <Component {...pageProps} />;
 }
